ebbdc89ec30e21d53c8b7b192dce66d9d4b15920
navaneeth <navaneeth@DESKTOP-M82B481>
2026-05-07 11:30:48 +0530

    feat: add login/logout auth with base template and sidebar

--- hostel_room/settings.py
+++ hostel_room/settings.py
@@ -119,3 +119,7 @@ STATIC_URL = 'static/'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
